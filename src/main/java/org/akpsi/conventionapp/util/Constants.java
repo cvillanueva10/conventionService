1650bbc8920a3b8cec73385bc4e33cdde00f8296
Adrian-Ryan Acala <me@adrianacala.com>
2016-11-06 19:54:43 -0800

    Added settings.

--- src/main/java/org/akpsi/conventionapp/util/Constants.java
+++ src/main/java/org/akpsi/conventionapp/util/Constants.java
@@ -2,6 +2,6 @@ package org.akpsi.conventionapp.util;
 
 public class Constants {
 
-	public static final String GET_TIMES = "select date, time, activity, description from times";
+	public static final String GET_TIMES = "select date, time, activity, description from times order by date, time asc";
 	
 }
