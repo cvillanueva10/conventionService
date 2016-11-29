c01a7d0de3865f7e126711ee41ca2f66a136a410
Adrian-Ryan Acala <adrianacala87@gmail.com>
2016-11-28 17:58:07 -0800

    Working.

--- src/main/java/org/akpsi/conventionapp/services/UsersService.java
+++ src/main/java/org/akpsi/conventionapp/services/UsersService.java
@@ -1,72 +1,72 @@
 package org.akpsi.conventionapp.services;
 
+import java.nio.charset.StandardCharsets;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
 import java.sql.Connection;
-import java.sql.ResultSet;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.LinkedList;
-import java.util.List;
 
 import javax.crypto.SecretKey;
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.PBEKeySpec;
 
-import org.akpsi.conventionapp.objects.Users;
 import org.akpsi.conventionapp.util.ConnectionFactory;
 import org.akpsi.conventionapp.util.Constants;
+import org.springframework.security.crypto.keygen.KeyGenerators;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UsersService {
-	
+
 	// http://stackoverflow.com/questions/18142745/how-do-i-generate-a-salt-in-java-for-salted-hash
 	// https://www.owasp.org/index.php/Hashing_Java
-	public static byte[] hashPassword( final char[] password, final byte[] salt, final int iterations, final int keyLength ) throws InvalidKeySpecException, NoSuchAlgorithmException {
-	   
-	   SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA512");
-	   PBEKeySpec spec = new PBEKeySpec(password, salt, iterations, keyLength);
-	   SecretKey key = skf.generateSecret(spec);
-	   byte[] res = key.getEncoded( );
-	   return res;
-   }
-	
-	@RequestMapping("/listUsers")
-	public List<Users> getUser() {
-		
-		// LinkedList of User objects named 'usersList'
-		List<Users> usersList = new LinkedList<Users>();
-		
+	public static String hashPassword( final char[] password, final byte[] salt, final int iterations, final int keyLength ) throws InvalidKeySpecException, NoSuchAlgorithmException {
+
+		SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA512");
+		PBEKeySpec spec = new PBEKeySpec(password, salt, iterations, keyLength);
+		SecretKey key = skf.generateSecret(spec);
+		byte[] res = key.getEncoded( );
+//		return res;
+		return new String(res, StandardCharsets.UTF_8);
+	}
+
+	@RequestMapping(value = "/createUser", method = RequestMethod.POST)
+	public String createUser(
+			@RequestParam (value = "email", required = false) String email,
+			@RequestParam (value = "password", required = false) String password,
+			@RequestParam (value = "phone_number", required = false) String phoneNumber,
+			@RequestParam (value = "address", required = false) String address,
+			@RequestParam (value = "city", required = false) String city,
+			@RequestParam (value = "state", required = false) String state,
+			@RequestParam (value = "zip", required = false) String zip
+			) throws InvalidKeySpecException, NoSuchAlgorithmException {
 		try(
 				Connection conn = ConnectionFactory.getConnection();
-				Statement stmt = conn.createStatement();
-				// calls query in string-form (GET_USERS) from Constants class
-				ResultSet rs = stmt.executeQuery(Constants.GET_USERS);
+				PreparedStatement ps = conn.prepareStatement(Constants.REGISTER_USER);
 				) {
-			while (rs.next()) {
-				Users user = new Users();
-				user.setEmail(rs.getString("email"));
-				//user.setSalt(rs.getString("salt"));
-				user.setSalt(rs.getString(hashPassword("password", "salt", 1, 256));
-				user.setPassword(rs.getString("password"));
-				user.setPhone_number(rs.getString("phone_number"));
-				user.setCreated_on(rs.getString("created_on"));
-				user.setEdited_on(rs.getString("edited_on"));
-				user.setAddress(rs.getString("address"));
-				user.setCity(rs.getString("city"));
-				user.setState(rs.getString("state"));
-				user.setZip(rs.getString("zip"));
-				// add new user info to list
-				usersList.add(user);
-			}
+			String salt = generateSalt();
+			ps.setString(1, email);
+			ps.setString(2, salt);
+			ps.setString(3, hashPassword(password.toCharArray(), salt.getBytes(), 1, 256));
+			ps.setString(4, phoneNumber);
+			ps.setString(5, address);
+			ps.setString(6, city);
+			ps.setString(7, state);
+			ps.setString(8, zip);
+			ps.execute();
 		}
 		catch(SQLException e) {
 			e.printStackTrace();
 		}
-		
-		return usersList;
+		return "userCreated";
+	}
+
+	private String generateSalt() {
+		return KeyGenerators.string().generateKey();
 	}
 
 }
--- src/main/java/org/akpsi/conventionapp/util/Constants.java
+++ src/main/java/org/akpsi/conventionapp/util/Constants.java
@@ -4,6 +4,7 @@ public class Constants {
 
 	public static final String GET_TIMES = "select date, time, activity, description from times order by date, time asc";
 	
-	public static final String GET_USERS = "SELECT email, salt, password, phone_number, created_on, edited_on, address, city, state, zip FROM users GROUP BY state, city ORDER BY email ASC"; 
+	public static final String GET_USERS = "SELECT email, salt, password, phone_number, created_on, edited_on, address, city, state, zip FROM users GROUP BY state, city ORDER BY email ASC";
 	
+	public static final String REGISTER_USER = "INSERT INTO `users` (`email`, `salt`, `password`, `phone_number`, `address`, `city`, `state`, `zip`) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
 }
