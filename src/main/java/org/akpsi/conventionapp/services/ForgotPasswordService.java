fbc745b6071726d76a66bbcb2987f941659d2b32
Chris Villanueva <cvillan0123@gmail.com>
2017-04-07 20:47:29 -0700

    Implemented ResetPassword function along with necessary subfunctions

--- src/main/java/org/akpsi/conventionapp/services/ForgotPasswordService.java
+++ src/main/java/org/akpsi/conventionapp/services/ForgotPasswordService.java
@@ -1,5 +1,7 @@
 package org.akpsi.conventionapp.services;
 
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -26,68 +28,139 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class ForgotPasswordService {
 
-	public static boolean userExist(String email){
-		try(Connection conn = ConnectionFactory.getConnection();
-				PreparedStatement ps = conn.prepareStatement(Constants.CHECK_FOR_USER)){
-			int numOfUserWithEmail = Integer.MIN_VALUE;
-			ps.setString(1, email);
-			try(ResultSet rs = ps.executeQuery()){
-				if (rs.next()){
-					numOfUserWithEmail = rs.getInt(1);
-				}
-			}
-			if (numOfUserWithEmail==1){
-				return true;
-			}
-			return false;
-		} catch (SQLException e) {
-			e.printStackTrace();
+
+	@RequestMapping(value = "/forgotpassword", method = RequestMethod.POST)
+	public Response forgotPassword(@RequestBody User user, HttpServletResponse response) {
+
+		if (!CommonServices.userExist(user.getEmail())){
+			return new Response(false,"No user is registered under this email.");
 		}
-		return true;
-	}
-	
-@RequestMapping(value = "/forgotpassword", method = RequestMethod.POST)
-public Response forgotPassword(@RequestBody User user, HttpServletResponse response) {
-	
-	if (!userExist(user.getEmail())){
-		return new Response(false,"No user is registered under this email.");
-	}
-	
-	try(
-			Connection conn = ConnectionFactory.getConnection();
-			PreparedStatement ps = conn.prepareStatement(Constants.FORGOT_PASSWORD_ENTRY);
-			){
-		String token = generateToken();
-		ps.setString(1,user.getEmail());
-		ps.setString(2, token);
-		
-		if(Integer.parseInt(user.getAllowEmail()) == 1){
+
+		try(
+				Connection conn = ConnectionFactory.getConnection();
+				PreparedStatement ps = conn.prepareStatement(Constants.SQL_FORGOT_PASSWORD_ENTRY);
+				){
+			String token = generateToken();
+			ps.setString(1,user.getEmail());
+			ps.setString(2, token);
+			ps.execute();
+
+			insertOldPassword(user);
+
 			HttpHeaders headers = new HttpHeaders();
 			RestTemplate restTemplate = new RestTemplate();
 			headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-	
+
 			MultiValueMap<String, String> map= new LinkedMultiValueMap<String, String>();
 			map.add("apiKey", "4hJctCGcfUNkEkJh9mH42yQ3Q9WHQhLv");
 			map.add("toAddress", user.getEmail());
 			map.add("subject", "Forgot Password - Link to reset password");
-			map.add("message", " (Insert message and link to reset user password)");
+			map.add("message", "<a href=\"http://localhost:8080/Convention/forgot_password?token=" + token + "\"> Click here to reset your password </a>");
 
 			HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<MultiValueMap<String, String>>(map, headers);
 
 			restTemplate.postForEntity(Constants.EMAIL_SERVICE_URL , request , String.class );
+
+			return new Response(true, "Forgot Password Session Started");
+
+		}catch(Exception e) {
+			return new Response(false, "Error creating session");
 		}
-		
-		
-		
-		
-		return new Response(true, "Forgot Password Session Started");
-		
-	}catch(Exception e) {
-		return new Response(false, "Error creating session");
+
 	}
-	
-}
-private String generateToken() {
-	return new String(KeyGenerators.string().generateKey());
-}
+
+	@RequestMapping(value = "/resetpassword", method = RequestMethod.POST)
+	public Response resetPassword(@RequestBody User user, HttpServletResponse response){
+		boolean isOldPassword = false;
+		boolean valid = isValidResetToken(user.getSessionId());
+		if(!valid){
+			return new Response(false, "Session has expired");
+		}
+
+		String salt = user.getSalt();
+		try {
+			String hashedNewPassword = RegisterUserService.hashPassword(user.getPassword().toCharArray(), salt.getBytes(), 1, 256);
+			isOldPassword = checkIfOldPassword(user, hashedNewPassword);
+		} catch (Exception e){
+			return new Response(false, "Error checking old passwords");
+		}
+		if(isOldPassword){
+			return new Response(false, "This is an old password");
+		}
+		 	
+		try(
+				Connection conn = ConnectionFactory.getConnection();
+				PreparedStatement ps = conn.prepareStatement(Constants.SQL_RESET_PASSWORD);
+				){
+			ps.setString(1, RegisterUserService.hashPassword(user.getPassword().toCharArray(), salt.getBytes(), 1, 256));
+			ps.setString(2, user.getUserId());
+			ps.execute();
+
+			return new Response(true);
+		}catch(Exception e){
+			return new Response(false, "Error resetting password");
+		}
+
+	}
+	private boolean isValidResetToken(String token){
+		try(
+				Connection conn = ConnectionFactory.getConnection();
+				PreparedStatement ps = conn.prepareStatement(Constants.SQL_CHECK_RESET_PASSWORD_SESSION);
+				){
+			ps.setString(1, token);
+			try(ResultSet rs = ps.executeQuery()){
+				while (rs.next()){
+					int value = rs.getInt("cnt");
+					if (value > 0){
+						return true;
+					}else{
+						return false;
+					}
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+	private boolean checkIfOldPassword(User user, String newPassword){
+		int numOfSameOldPasswords = 0;
+		try(		
+				Connection conn = ConnectionFactory.getConnection();
+				PreparedStatement ps = conn.prepareStatement(Constants.SQL_CHECK_OLD_PASSWORDS);
+				){
+			ps.setString(1, user.getEmail());
+			ps.setString(2, newPassword);
+			try(ResultSet rs = ps.executeQuery()){
+				if(rs.next()){
+					numOfSameOldPasswords = rs.getInt("cnt");
+				}
+			}
+			if(numOfSameOldPasswords > 0){
+				return true;
+			}
+			return false;
+		} catch (Exception e){
+			return false;
+		}
+	}
+	private boolean insertOldPassword(User user){
+		try(
+				Connection conn = ConnectionFactory.getConnection();
+				PreparedStatement ps = conn.prepareStatement(Constants.SQL_INSERT_OLD_PASSWORD);
+				){
+			ps.setString(1, user.getUserId());
+			ps.setString(2, user.getSalt());
+			ps.setString(3, user.getPassword());
+			ps.execute();
+
+			return true;
+		}catch(Exception e){
+			return false;
+		}
+	}
+
+	private String generateToken() {
+		return new String(KeyGenerators.string().generateKey());
+	}
 }
--- src/main/java/org/akpsi/conventionapp/util/Constants.java
+++ src/main/java/org/akpsi/conventionapp/util/Constants.java
@@ -30,5 +30,13 @@ public class Constants {
 	
 	public static final String EMAIL_REGISTRATION_BODY = "Congratulations! You have successfully registered to volunteer for Convention!";
 	
-	public static final String FORGOT_PASSWORD_ENTRY = "INSERT INTO 'forgot_password' ('user_id','token') VALUES (?, ?)";
+	public static final String SQL_FORGOT_PASSWORD_ENTRY = "INSERT INTO 'forgot_password' ('user_id','token') VALUES (?, ?)";
+	
+	public static final String SQL_CHECK_RESET_PASSWORD_SESSION = "select count(token) as 'cnt' from forgot_password where token = ?";
+	
+	public static final String SQL_RESET_PASSWORD = "UPDATE users SET password = ? WHERE userId = ?";
+	
+	public static final String SQL_INSERT_OLD_PASSWORD = "INSERT INTO 'old_passwords' ('userId, salt, password) VALUES (?,?,?)";
+	
+	public static final String SQL_CHECK_OLD_PASSWORDS = "select count(password) as 'cnt' from old_passwords where userId = ? and password - ?";
 }
